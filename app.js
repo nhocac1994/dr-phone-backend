055b8754847f2f15f96d095607c3d53e2ea06ef0
Quyền Nguyễn <quyennguyen@MacBook-Pro-cua-Quyen.local>
2025-07-04 10:12:43 +0700

    Move app.js to root and update Node.js version

---
+++ app.js
@@ -0,0 +1,7 @@
+// Debug current directory
+console.log('Current directory:', process.cwd());
+console.log('__dirname:', __dirname);
+console.log('File location:', __filename);
+
+// Import app from src directory
+require('./src/app.js'); 
